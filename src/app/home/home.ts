22c19a21735bce3c848ff7dc8ed839f75dac3c3e
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-05 21:33:22 +0200

    feat: Move reviews to component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { Certificates } from "./certificates";
 import { Featured } from "./featured";
+import { Reviews } from "./reviews";
 @Component ({
     selector: "home-page",
     template: `
@@ -17,43 +18,10 @@ import { Featured } from "./featured";
     </div>
     <certificates/>
     <featured/>
-    
-    <div class="reviews-container">
-        <div class="reviews-head">
-            <h2>Customer Top Picks</h2>
-        </div>
-        <div class="reviews-box">
-            <ul class="reviews-list">
-                <li class="reviews-item">
-                    <div class="reviews-image-container">
-                        <img class="reviews-image" src="https://static.photos/640x360/20" alt="product image">
-                    </div>
-                    <div class="reviews-text">
-                        <h2>Product Name</h2>
-                        <p>Stars Placeholder</p>
-                        <p>"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"</p>
-                        <p>Reviewer's name</p>
-                        <button>Browse product category</button>
-                    </div>
-                </li>
-                <li class="reviews-item">
-                    <div class="reviews-image-container">
-                        <img class="reviews-image" src="https://static.photos/640x360/20" alt="product image">
-                    </div>
-                    <div class="reviews-text">
-                        <h2>Product Name</h2>
-                        <p>Stars Placeholder</p>
-                        <p>"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"</p>
-                        <p>Reviewer's name</p>
-                        <button>Browse product category</button>
-                    </div>
-                </li>
-            </ul>    
-        </div>
-    </div>
+    <reviews/>
     `,
     styleUrl: './home.css',
-    imports: [RouterLink, Certificates, Featured]
+    imports: [RouterLink, Certificates, Featured, Reviews]
 })
 
 export class HomePage {}
---
+++ src/app/home/reviews.ts
@@ -0,0 +1,46 @@
+import { Component } from "@angular/core";
+
+
+
+@Component ({
+    selector: 'reviews',
+    template: `
+    <div class="reviews-container">
+        <div class="reviews-head">
+            <h2>Customer Top Picks</h2>
+        </div>
+        <div class="reviews-box">
+            <ul class="reviews-list">
+                <li class="reviews-item">
+                    <div class="reviews-image-container">
+                        <img class="reviews-image" src="https://static.photos/640x360/20" alt="product image">
+                    </div>
+                    <div class="reviews-text">
+                        <h2>Product Name</h2>
+                        <p>Stars Placeholder</p>
+                        <p>"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"</p>
+                        <p>Reviewer's name</p>
+                        <button>Browse product category</button>
+                    </div>
+                </li>
+                <li class="reviews-item">
+                    <div class="reviews-image-container">
+                        <img class="reviews-image" src="https://static.photos/640x360/20" alt="product image">
+                    </div>
+                    <div class="reviews-text">
+                        <h2>Product Name</h2>
+                        <p>Stars Placeholder</p>
+                        <p>"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"</p>
+                        <p>Reviewer's name</p>
+                        <button>Browse product category</button>
+                    </div>
+                </li>
+            </ul>    
+        </div>
+    </div>`,
+    styleUrl: './home.css'
+})
+
+export class Reviews { 
+
+}
