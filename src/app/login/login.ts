be3baf74410a4731cbf55ac1726672a1b639ddbd
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-06 12:51:08 +0200

    feat: Login layout and styling

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -3,10 +3,32 @@ import { Component } from "@angular/core";
 
 @Component ({
     selector: 'login',
-    template: ``,
+    template: `
+    <div class="login-wrapper">
+        <img src="" alt="Logo">
+        <div class="welcome-wrapper">
+            <span>Welcome Back!</span>
+            <h2>Log in</h2>
+        </div>
+        <div class="input-wrapper">
+            <span>Email</span>
+            <input type="text">
+            <div class="password-wrapper">
+                <span>Password</span>
+                <a href="">Forgot Password?</a>
+            </div>
+            <input type="text">
+            <button id="login-button">Log in</button>
+            <span id="signup-span">Don't have an account? <a href="">Sign Up</a></span>
+        </div>
+    </div>
+    <div>
+
+    </div>
+    `,
     styleUrl: './login.css',
 })
 
 export class Login {
-    
+
 }
