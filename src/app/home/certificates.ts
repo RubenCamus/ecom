c92f4cab10ee403253d2fdce50353babb20ba1b4
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-06 11:45:56 +0200

    fix: Certificate bar layout

--- src/app/home/certificates.ts
+++ src/app/home/certificates.ts
@@ -5,22 +5,29 @@ import { Component } from "@angular/core";
     selector: 'certificates',
     template: `
         <div class="certificates-bar">
-        <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
-        <div class="certificate-box">
-            <p class="certificate-title">Lorem ipsum</p>
-            <p>Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
-        </div>
-        <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
-        <div class="certificate-box">
-            <p class="certificate-title">Lorem ipsum</p>
-            <p>Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
-        </div>
-        <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
-        <div class="certificate-box">
-            <p class="certificate-title">Lorem ipsum</p>
-            <p>Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
-        </div>
-    </div>`,
+            <div class="certificate-box">
+                <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
+                <div class="certificate-text-wrapper">
+                    <p class="certificate-title">FREE SHIPPING</p>
+                    <p>When you spend 50.00$ minimum on all online orders.</p>
+                    <span>DETAILS</span>
+                </div>
+            </div>
+            <div class="certificate-box">
+                <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
+                <div class="certificate-text-wrapper">
+                    <p class="certificate-title">PRICE MATCHING</p>
+                    <p>Present a qualifying competitor's ad and we'll gladly price match!</p>
+                </div>
+            </div>
+            <div class="certificate-box">
+                <img class="certificate-image" src="https://static.photos/640x360/1" alt="certificate-icon">
+                <div class="certificate-text-wrapper">
+                    <p class="certificate-title">STORE PICKUP</p>
+                    <p>Buy online, and then pick up your order in store or curbside.</p>
+                </div>
+            </div>
+        </div>`,
     styleUrl: './home.css',
 })
 
