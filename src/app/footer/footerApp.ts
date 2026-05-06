99f7af312d44340cedf5edc2867c20597eaaa8a2
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-06 17:00:53 +0200

    feat: Style footer and set up layout placeholders

--- src/app/footer/footerApp.ts
+++ src/app/footer/footerApp.ts
@@ -5,26 +5,26 @@ import { Component } from "@angular/core";
     selector: 'footer-app',
     template: `
     <footer>
-        <div>
-            <div class="socials">
-                <img src="" alt="Twitter Logo">
-                <img src="" alt="Instagram Logo">
-                <img src="" alt="Facebook Logo">
+        <div class="group-a">
+            <div class="socials-wrapper">
+                <a href=""><img src="" alt="Twitter Logo"></a>
+                <a href=""><img src="" alt="Instagram Logo"></a>
+                <a href=""><img src="" alt="Facebook Logo"></a>
                 <a href="">#TestShop</a>
             </div>
             <div class="policies-wrapper">
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+                <a href="">Home</a>
+                <a href="">About Us</a>
+                <a href="">Work With Us</a>
+                <a href="">Customer Service</a>
+                <a href="">Gift Cards</a>
+                <a href="">Contact Us</a>
+                <a href="">Sign Up</a>
+                <a href="">Order Tracking</a>
+                <a href="">Return Policy</a>
             </div>
         </div>
-        <div>
+        <div class="group-b">
             <h2>STORE LOCATIONS</h2>
             <div class="stores-wrapper">
                 <div class="store">
