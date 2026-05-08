dd32b949a068e88d1f69e7add6632dc1d6b12cfa
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-08 11:33:51 +0200

    feat: Install Angular Material

---
+++ src/app/layouts/mainLayout.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import { HomePage } from '../home/home';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import { NavBar } from '../nav/navBar';
+import { Cart } from "../cart/cart";
+import { FooterApp } from '../footer/footerApp';
+
+@Component({
+  selector: 'main-layout',
+  imports: [RouterLink, RouterOutlet, NavBar, Cart, FooterApp],
+  template: `
+    <nav-bar (toggle)="toggleCart()">
+    </nav-bar>
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name"></header>
+    </a>
+    <section class="content">
+      <router-outlet/>
+    </section>
+    @if (mostrarCarrito)  {
+      <cart (close)="toggleCart()"/>
+    }
+  </main>
+  <footer-app/>
+  `,
+  styleUrls: ['./mainLayout.css'],
+})
+export class MainLayout {
+  mostrarCarrito = false;
+
+  toggleCart() { 
+    this.mostrarCarrito = !this.mostrarCarrito;
+  }
+
+}
