7fe0bbe61b05ca0cb708cae9ccf25bb394d2e042
RubenCamus <Ruben-4-millet@hotmail.com>
2026-05-12 13:36:31 +0200

    fix: signup & login layout and css fixes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -5,26 +5,29 @@ import { RouterLink } from "@angular/router";
 @Component ({
     selector: 'login',
     template: `
-    <div class="login-wrapper">
-        <a routerLink="/"><img width="100px" src="/favicon.ico" alt="Logo"></a>
-        <div class="welcome-wrapper">
-            <span>Welcome Back!</span>
-            <h2>Log in</h2>
-        </div>
-        <div class="input-wrapper">
-            <label>Email</label>
-            <input type="email">
-            <div class="password-wrapper">
-                <label>Password</label>
-                <a href="">Forgot Password?</a>
+    <div class="screen-wrapper">
+        <form action="">
+            <div class="login-wrapper">
+                <a routerLink="/"><img width="100px" src="/favicon.ico" alt="Logo"></a>
+                <div class="welcome-wrapper">
+                    <h2>Create an account</h2>
+                </div>
+                <div class="input-wrapper">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" required>
+\
+                    <label for="psw">Password</label>
+                    <input type="password" name="psw" required>
+                    <div class="button-wrapper">
+                        <button id="login-button">Log in</button>
+                    </div>
+                    <label id="signup-span">Don't have an account? <a routerLink="/auth/signup">Sign Up</a></label>
+                </div>
             </div>
-            <input type="password">
-            <button id="login-button">Log in</button>
-            <label id="signup-span">Don't have an account? <a routerLink="/auth/signup">Sign Up</a></label>
+        </form>
+        <div>
+            <img id="background-image" src="/login-bg.jpg" alt="background">
         </div>
-    </div>
-    <div>
-
     </div>
     `,
     styleUrl: './login.css',
--- src/app/login/signup.ts
+++ src/app/login/signup.ts
@@ -5,29 +5,31 @@ import { RouterLink } from "@angular/router";
 @Component ({
     selector: 'login',
     template: `
-    <div class="login-wrapper">
-        <a routerLink="/"><img width="100px" src="/favicon.ico" alt="Logo"></a>
-        <div class="welcome-wrapper">
-            <span>Welcome!</span>
-            <h2>Sign up</h2>
-        </div>
-        <div class="input-wrapper">
-            <label>Email</label>
-            <input type="email">
-            <label>Confirm email</label>
-            <input type="email">
-            <div class="password-wrapper">
-                <label>Password</label>
-            </div>
-            <input type="password">
-            <label >Confirm password</label>
-            <input type="password">
-            <button id="signup-button">Sign up</button>
-            <label id="login-span">Already have an acount? <a routerLink="/auth/login">Log in</a></label>
-        </div>
-    </div>
-    <div>
+    <div class="screen-wrapper">
+        <form action="">
+            <div class="login-wrapper">
+                <a routerLink="/"><img width="100px" src="/favicon.ico" alt="Logo"></a>
+                <div class="welcome-wrapper">
+                    <h2>Create an account</h2>
+                </div>
+                <div class="input-wrapper">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" required>
+\
+                    <label for="psw">Password</label>
+                    <input type="password" name="psw" required>
 
+                    <label for="psw-repeat">Confirm Password</label>
+                    <input type="password" name="psw-repeat" required>
+                    <p>By creating an account you agree to our <a href="">Terms & Privacy</a></p>
+                    <div class="button-wrapper">
+                        <button id="signup-button">Sign Up</button>
+                    </div>
+                    <label id="login-span">Already have an account? <a routerLink="/auth/login">Log in</a></label>
+                </div>
+            </div>
+        </form>
+        <img id="background-image" src="/login-bg.jpg" alt="background">
     </div>
     `,
     styleUrl: './login.css',
